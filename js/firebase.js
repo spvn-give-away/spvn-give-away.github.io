4a198bfd59ac0b03607799ba575af4aeb0543b80
spvn-give-away <alphapasai@gmail.com>
2026-02-03 17:49:19 +0530

    Update Firebase configuration with new project details

--- js/firebase.js
+++ js/firebase.js
@@ -1,8 +1,13 @@
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
+  apiKey: "AIzaSyCjeK2-hXcsxQ25QCWaSYt1mzS_mWdrG2E",
+  authDomain: "project-1640118831593628871.firebaseapp.com",
+  projectId: "project-1640118831593628871",
+  storageBucket: "project-1640118831593628871.firebasestorage.app",
+  messagingSenderId: "1014217672368",
+  appId: "1:1014217672368:web:f2e28931956bceae055d75",
+  measurementId: "G-2CZHNH3DNB"
 };
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
+
